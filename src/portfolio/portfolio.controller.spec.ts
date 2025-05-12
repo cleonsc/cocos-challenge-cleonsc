771b1d07cd72ae5956cb4e0fd3e48fc0ea1992d2
Cleonsc <cschaus@agropago.com>
2025-05-12 20:30:45 -0300

    [add] tests of portfolio controller

--- src/portfolio/portfolio.controller.spec.ts
+++ src/portfolio/portfolio.controller.spec.ts
@@ -4,13 +4,14 @@ import { PortfolioService } from './portfolio.service';
 
 describe('PortfolioController', () => {
   let controller: PortfolioController;
+  let service: PortfolioService;
+
+
+  const mockPortfolioService = {
+    getPortfolio: jest.fn(),
+  };
 
   beforeEach(async () => {
-    const mockPortfolioService = {
-      // Agrega aquí los métodos mock necesarios para las pruebas
-      getPortfolio: jest.fn(),
-    };
-
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PortfolioController],
       providers: [
@@ -22,9 +23,44 @@ describe('PortfolioController', () => {
     }).compile();
 
     controller = module.get<PortfolioController>(PortfolioController);
+    service = module.get<PortfolioService>(PortfolioService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('getPortfolio', () => {
+    it('should call PortfolioService.getPortfolio with the correct userId', async () => {
+      const mockPortfolio = {
+        userId: 1,
+        totalValue: 1000,
+        availableCash: 500,
+        positions: [],
+      };
+      mockPortfolioService.getPortfolio.mockResolvedValue(mockPortfolio);
+
+      const userId = 1;
+      const result = await controller.getPortfolio(userId);
+
+      expect(service.getPortfolio).toHaveBeenCalledWith(userId);
+      expect(result).toEqual(mockPortfolio);
+    });
+
+    it('should return an empty portfolio if the service returns an empty result', async () => {
+      const mockPortfolio = {
+        userId: 2,
+        totalValue: 0,
+        availableCash: 0,
+        positions: [],
+      };
+      mockPortfolioService.getPortfolio.mockResolvedValue(mockPortfolio);
+
+      const userId = 2;
+      const result = await controller.getPortfolio(userId);
+
+      expect(service.getPortfolio).toHaveBeenCalledWith(userId);
+      expect(result).toEqual(mockPortfolio);
+    });
   });
 });
